efead5fe75f7dd2daca153484e8d308adbc32b93
Yuichi Yogo <yuichkun@gmail.com>
2021-03-07 06:44:47 +0900

    add serialization method for models

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -4,8 +4,12 @@ from django.db.models import Model, CharField, DateTimeField, ForeignKey, Intege
 class Question(Model):
     question_text = CharField(max_length=200)
     pub_date = DateTimeField('date published')
+    def __str__(self):
+        return self.question_text
 
 class Choice(Model):
     question = ForeignKey(Question, on_delete=CASCADE)
     choice_text = CharField(max_length=200)
     votes = IntegerField(default=0)
+    def __str__(self):
+        return self.choice_text
