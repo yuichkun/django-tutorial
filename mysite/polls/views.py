7f1f41058810a8baeeedc98057f44a446a073b3f
Yuichi Yogo <yuichkun@gmail.com>
2021-03-07 07:12:35 +0900

    create polls detail page

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -6,3 +6,8 @@ def index(request):
     import datetime
     now = datetime.datetime.now().strftime('%H:%M:%S')
     return HttpResponse("Hello, world. You're at the polls index." + now)
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question {}.".format(question_id))
+
+
