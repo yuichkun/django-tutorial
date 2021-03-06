3a1c89107812efee9d6da4d717e6eda7aeb166d1
Yuichi Yogo <yuichkun@gmail.com>
2021-03-07 03:45:48 +0900

    print out current time

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -3,4 +3,6 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    import datetime
+    now = datetime.datetime.now().strftime('%H:%M:%S')
+    return HttpResponse("Hello, world. You're at the polls index." + now)
