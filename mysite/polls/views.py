65b7a6d3ceee688a9643fd0c27c2821306ec27f1
Yuichi Yogo <yuichkun@gmail.com>
2021-03-07 10:41:32 +0900

    use F function in vote to avoid race condition

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
+from django.db.models import F
 from .models import Question, Choice
 
 def index(request):
@@ -28,6 +29,6 @@ def vote(request, question_id):
             'error_message': e
         })
     else:
-        selected_choice.votes += 1
+        selected_choice.votes = F('votes') + 1
         selected_choice.save()
         return HttpResponseRedirect(reverse('polls:result', args=(question.id,)))
