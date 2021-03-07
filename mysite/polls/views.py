b473729c2a9a41249922f874942629e47a3e7492
Yuichi Yogo <yuichkun@gmail.com>
2021-03-07 10:34:33 +0900

    impl voting function

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,6 +1,7 @@
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
-from .models import Question
+from django.urls import reverse
+from .models import Question, Choice
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
@@ -18,4 +19,15 @@ def result(request, question_id):
     return HttpResponse(response.format(question_id))
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on {}.".format(question_id))
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except(KeyError, Choice.DoesNotExist) as e:
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': e
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:result', args=(question.id,)))
