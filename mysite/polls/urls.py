725ff7af529bc122f0402771e2ce54f488a06573
Yuichi Yogo <yuichkun@gmail.com>
2021-03-07 10:55:38 +0900

    refactor polls views using generic views

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -3,9 +3,9 @@ from . import views
 
 app_name = 'polls'
 urlpatterns = [
-    path('', views.index,  name="index"),
-    path('<int:question_id>/', views.detail, name="detail"),
-    path('<int:question_id>/results', views.result, name="result"),
+    path('', views.IndexView.as_view(),  name="index"),
+    path('<int:pk>/', views.DetailView.as_view(), name="detail"),
+    path('<int:pk>/results', views.ResultView.as_view(), name="result"),
     path('<int:question_id>/vote/', views.vote, name="vote"),
 ]
 
